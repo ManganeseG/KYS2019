3fc4a1c4728742db758beff73338b7c3577bd698
ManganeseG <celinebrasseurcbr@gmail.com>
2019-10-05 16:53:22 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/ManganeseG/KYS2019""
    This reverts commit 708460c62881faccce6123e12d6d58ab695eed9b.

---
+++ PirateGame/Assets/Scripts/HUD/ShipCooldown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipCooldown : MonoBehaviour
+{
+    private Slider slider;
+    private float startTimer = 0f;
+    public float GameTime = 60f;
+
+
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        startTimer += Time.deltaTime / GameTime;
+        slider.value = startTimer;
+
+        if (startTimer >= 1f)
+        {
+            startTimer = 0f;
+            //GameEnd
+        }
+    }
+}
